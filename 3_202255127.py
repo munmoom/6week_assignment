c576e6430cfaba975d3e2d19d96306cc510d9d2a
munmoom <100830462+munmoom@users.noreply.github.com>
2022-04-12 16:44:56 +0900

    Colaboratory를 통해 생성됨

---
+++ 3_202255127.py
@@ -0,0 +1,58 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "3-202255127.",
+      "provenance": [],
+      "authorship_tag": "ABX9TyNkIjh/os28fFNClT4gmtTr",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/munmoom/6week_assignment/blob/main/3_202255127.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "def solve(a):\n",
+        "  return sum(a)"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "DfJuS06StYNS",
+        "outputId": "c37dc4a9-466e-439b-95a8-8cb076cf2473"
+      },
+      "execution_count": 10,
+      "outputs": [
+        {
+          "output_type": "execute_result",
+          "data": {
+            "text/plain": [
+              "int"
+            ]
+          },
+          "metadata": {},
+          "execution_count": 10
+        }
+      ]
+    }
+  ]
+}
